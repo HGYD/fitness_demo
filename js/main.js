60b1a1b6d937d7a537d196cdc58becea528db08d
bobhe <bobhe@lexinfinteach.com>
2017-08-22 10:51:19 +0800

    v0.06

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
 $(document).ready(function(){
    $('.ui.dropdown').dropdown();
 });
+
+$(function() {
+
+    $("h2")
+        .wrapInner("<span>")
+
+    $("h2 br")
+        .before("<span class='spacer'>")
+        .after("<span class='spacer'>");
+
+});
+
+$(function() {
+
+    $("h3")
+        .wrapInner("<span>")
+
+    $("h2")
+        .before("<span class='spacer'>")
+        .after("<span class='spacer'>");
+
+});
